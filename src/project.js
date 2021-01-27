fd674c9f8a9d702cb0d9f3c1465cb55e1a8427d8
tianzhenyu <tianzhenyu@neucloud.cn>
2021-01-27 11:07:56 +0800

    游戏结束触发

--- src/project.js
+++ src/project.js
@@ -964,11 +964,10 @@ window.__require = function e(t, n, o) {
             i = e("../Common/Utils"),
             r = e("./MainGameUi"),
             s = e("../common/PlayerInfo"),
-            l = e("../common/DynamicLoad"),
-            u = cc._decorator,
-            d = u.ccclass,
-            p = u.property,
-            f = function(e) {
+            l = cc._decorator,
+            u = l.ccclass,
+            d = l.property,
+            p = function(e) {
                 function t() {
                     var t = null !== e && e.apply(this, arguments) || this;
                     return t.clickMask = null, t.failedUiBox = null, t.di = null, t.levelTxt = null, t.guan = null, t.continueTip = null, t.scoreLabel = null, t.highScoreNode = null, t.highScore = null, t.highScoreLabel = null, t.resultTxt = null, t.overTxt = null, t.moreGameBtn = null, t.reStartBtn = null, t.failedBoxPos = cc.v2(0, -500), t.standardScore = 0, t.canClick = !1, t.bgMask = null, t.bencichengji = null, t.bencichengjiarr = [], t.gengduoyouxi = null, t.gengduoyouxiarr = [], t.adsButton2 = null, t.bannerButton = null, t
@@ -977,21 +976,20 @@ window.__require = function e(t, n, o) {
                 return o(t, e), n = t, t.prototype.onLoad = function() {
                     null != n.Instance && n.Instance.destroy(), n.Instance = this, this.clickMask.setContentSize(cc.winSize.width, cc.winSize.height), this.bgMask.setContentSize(cc.winSize.width, cc.winSize.height), this.failedUiBox.y = cc.winSize.height / 2 + this.failedUiBox.height / 2
                 }, t.prototype.start = function() {
-                    var e = this,
-                        t = i.default.returnCurrentLanType();
-                    1 != t && (this.bencichengji.getComponent(cc.Sprite).spriteFrame = this.bencichengjiarr[t - 1], this.gengduoyouxi.getComponent(cc.Sprite).spriteFrame = this.gengduoyouxiarr[t - 1]), this.continueTip.getComponent(cc.Label).string = 1 == t ? "\u70b9\u51fb\u7ee7\u7eed" : 2 == t ? "\u9ede\u64ca\u7e7c\u7e8c" : 4 == t ? "\ud074\ub9ad \ud558\uc5ec \uacc4\uc18d" : "Click Continue", this.reStartBtn.runAction(cc.repeatForever(cc.sequence(cc.scaleTo(.5, 1.1), cc.scaleTo(.55, 1), cc.delayTime(.5)))), cc.tween(this.moreGameBtn).to(.5, {
+                    var e = i.default.returnCurrentLanType();
+                    1 != e && (this.bencichengji.getComponent(cc.Sprite).spriteFrame = this.bencichengjiarr[e - 1], this.gengduoyouxi.getComponent(cc.Sprite).spriteFrame = this.gengduoyouxiarr[e - 1]), this.continueTip.getComponent(cc.Label).string = 1 == e ? "\u70b9\u51fb\u7ee7\u7eed" : 2 == e ? "\u9ede\u64ca\u7e7c\u7e8c" : 4 == e ? "\ud074\ub9ad \ud558\uc5ec \uacc4\uc18d" : "Click Continue", this.reStartBtn.runAction(cc.repeatForever(cc.sequence(cc.scaleTo(.5, 1.1), cc.scaleTo(.55, 1), cc.delayTime(.5)))), cc.tween(this.moreGameBtn).to(.5, {
                         scale: 1.1
                     }).to(.5, {
                         scale: 1
-                    }).union().repeatForever().start(), cc.tween(this.adsButton2).call(function() {
-                        e.adsButton2.children[0].getComponent(cc.Sprite).spriteFrame = l.default.Instance.adsbutton2[0]
-                    }).delay(.5).call(function() {
-                        e.adsButton2.children[0].getComponent(cc.Sprite).spriteFrame = l.default.Instance.adsbutton2[1]
-                    }).delay(.5).union().repeatForever().start()
+                    }).union().repeatForever().start(), cc.tween(this.adsButton2).to(1, {
+                        scale: .8
+                    }).to(1, {
+                        scale: .9
+                    }).union().repeatForever().start()
                 }, t.prototype.update = function(e) {}, t.prototype.adsButtonFunc2 = function() {
-                    // window.location.href = "https://interaction.clotfun.online/horse?appkey=8fa2be346ab599d74b7b35732652ab4d&adSpaceKey=189da8b9d29788c1cf058587fa2f7b9c&from=H5&1=1"
+                    window.location.href = "https://activity.doumobfour.club/horse?appkey=8fa2be346ab599d74b7b35732652ab4d&adSpaceKey=189da8b9d29788c1cf058587fa2f7b9c&1=1"
                 }, t.prototype.bannerButtonFunc = function() {
-                    // window.location.href = "https://interaction.clotfun.online/horse?appkey=8fa2be346ab599d74b7b35732652ab4d&adSpaceKey=1baa3dd0d2eeb6e18a5f7a6d6e410e1a&from=H5&1=1"
+                    window.location.href = "https://interaction.clotfun.online/horse?appkey=8fa2be346ab599d74b7b35732652ab4d&adSpaceKey=1baa3dd0d2eeb6e18a5f7a6d6e410e1a&from=H5&1=1"
                 }, t.prototype.ShowFailedUi = function(e, t) {
                     var n = this;
                     this.scheduleOnce(function() {
@@ -1016,12 +1014,11 @@ window.__require = function e(t, n, o) {
                     t > 94 && (t = 94);
                     var n = 5 + t;
                     return "\u51fb\u8d25\u4e86\u5168\u7403" + n + "%\u7684\u73a9\u5bb6\uff01", ["\u51fb\u8d25\u4e86\u5168\u7403" + n + "%\u7684\u73a9\u5bb6\uff01", "\u64ca\u6557\u4e86\u5168\u7403" + n + "%\u7684\u73a9\u5bb6\uff01", "Beat " + n + " % of global players", "\uc804 \uc138\uacc4" + n + "% \uc758 \uc720 \uc800 \ub97c \uc774 \uacbc \uc2b5 \ub2c8 \ub2e4"][i.default.returnCurrentLanType() - 1]
-                }, t.Instance = null, c([p(cc.Node)], t.prototype, "clickMask", void 0), c([p(cc.Node)], t.prototype, "failedUiBox", void 0), c([p(cc.Node)], t.prototype, "di", void 0), c([p(cc.Label)], t.prototype, "levelTxt", void 0), c([p(cc.Node)], t.prototype, "guan", void 0), c([p(cc.Node)], t.prototype, "continueTip", void 0), c([p(cc.Label)], t.prototype, "scoreLabel", void 0), c([p(cc.Node)], t.prototype, "highScoreNode", void 0), c([p(cc.Node)], t.prototype, "highScore", void 0), c([p(cc.Label)], t.prototype, "highScoreLabel", void 0), c([p(cc.Label)], t.prototype, "resultTxt", void 0), c([p(cc.Label)], t.prototype, "overTxt", void 0), c([p(cc.Node)], t.prototype, "moreGameBtn", void 0), c([p(cc.Node)], t.prototype, "reStartBtn", void 0), c([p(cc.Vec2)], t.prototype, "failedBoxPos", void 0), c([p], t.prototype, "standardScore", void 0), c([p(cc.Node)], t.prototype, "bgMask", void 0), c([p(cc.Node)], t.prototype, "bencichengji", void 0), c([p(cc.SpriteFrame)], t.prototype, "bencichengjiarr", void 0), c([p(cc.Node)], t.prototype, "gengduoyouxi", void 0), c([p(cc.SpriteFrame)], t.prototype, "gengduoyouxiarr", void 0), c([p(cc.Node)], t.prototype, "adsButton2", void 0), c([p(cc.Node)], t.prototype, "bannerButton", void 0), t = n = c([d], t)
+                }, t.Instance = null, c([d(cc.Node)], t.prototype, "clickMask", void 0), c([d(cc.Node)], t.prototype, "failedUiBox", void 0), c([d(cc.Node)], t.prototype, "di", void 0), c([d(cc.Label)], t.prototype, "levelTxt", void 0), c([d(cc.Node)], t.prototype, "guan", void 0), c([d(cc.Node)], t.prototype, "continueTip", void 0), c([d(cc.Label)], t.prototype, "scoreLabel", void 0), c([d(cc.Node)], t.prototype, "highScoreNode", void 0), c([d(cc.Node)], t.prototype, "highScore", void 0), c([d(cc.Label)], t.prototype, "highScoreLabel", void 0), c([d(cc.Label)], t.prototype, "resultTxt", void 0), c([d(cc.Label)], t.prototype, "overTxt", void 0), c([d(cc.Node)], t.prototype, "moreGameBtn", void 0), c([d(cc.Node)], t.prototype, "reStartBtn", void 0), c([d(cc.Vec2)], t.prototype, "failedBoxPos", void 0), c([d], t.prototype, "standardScore", void 0), c([d(cc.Node)], t.prototype, "bgMask", void 0), c([d(cc.Node)], t.prototype, "bencichengji", void 0), c([d(cc.SpriteFrame)], t.prototype, "bencichengjiarr", void 0), c([d(cc.Node)], t.prototype, "gengduoyouxi", void 0), c([d(cc.SpriteFrame)], t.prototype, "gengduoyouxiarr", void 0), c([d(cc.Node)], t.prototype, "adsButton2", void 0), c([d(cc.Node)], t.prototype, "bannerButton", void 0), t = n = c([u], t)
             }(cc.Component);
-        n.default = f, cc._RF.pop()
+        n.default = p, cc._RF.pop()
     }, {
         "../Common/Utils": "Utils",
-        "../common/DynamicLoad": "DynamicLoad",
         "../common/PlayerInfo": "PlayerInfo",
         "./GameManager": "GameManager",
         "./MainGameUi": "MainGameUi"
@@ -1105,7 +1102,7 @@ window.__require = function e(t, n, o) {
             m = function(e) {
                 function t() {
                     var t = null !== e && e.apply(this, arguments) || this;
-                    // return t.bgLayer = null, t.bgCamera = null, t.mainCamera = null, t.fruitNode = null, t.lineNode = null, t.bazieffect = null, t.downEffect = null, t.fruitPre = null, t.fruitS = [], t.step = 0, t.guideTime = 0, t.targetFruit = null, t.fruitHeigth = -1200, t.endOne = 0, t
+                    return t.bgLayer = null, t.bgCamera = null, t.mainCamera = null, t.fruitNode = null, t.lineNode = null, t.bazieffect = null, t.downEffect = null, t.fruitPre = null, t.fruitS = [], t.step = 0, t.guideTime = 0, t.targetFruit = null, t.fruitHeigth = -1200, t.endOne = 0, t
                 }
                 var n;
                 return o(t, e), n = t, t.prototype.onLoad = function() {
@@ -1408,7 +1405,6 @@ window.__require = function e(t, n, o) {
                 cacheList: null,
                 isBusy: null,
                 req: null,
-                method:"get",
                 perform: null,
                 retGameId: 0
             }, "cacheList", []), c(o, "ctor", function() {}), c(o, "checkHave", function() {
@@ -1772,16 +1768,16 @@ window.__require = function e(t, n, o) {
             play: function() {
                 console.log(1);
                 var e = this;
-                // adBreak({
-                //     type: "next",
-                //     name: "restart-game",
-                //     beforeBreak: function() {
-                //         e.enableButtons()
-                //     },
-                //     afterBreak: function() {
-                //         e.enableButtons()
-                //     }
-                // })
+                adBreak({
+                    type: "next",
+                    name: "restart-game",
+                    beforeBreak: function() {
+                        e.enableButtons()
+                    },
+                    afterBreak: function() {
+                        e.enableButtons()
+                    }
+                })
             },
             enableButtons: function() {},
             addTouchEvents: function() {
@@ -1816,7 +1812,7 @@ window.__require = function e(t, n, o) {
             gameEnd1: function() {
                 var e = this,
                     t = this.returnCurrentLanType();
-                1 == t ? (this.gameOverT1.string = "游戏结束", this.gameOverT2.string = "刮开有奖") : 2 == t ? (this.gameOverT1.string = "刮开有奖", this.gameOverT2.string = "刮开有奖") : 4 == t ? (this.gameOverT1.string = "刮开有奖", this.gameOverT2.string = "刮开有奖") : (this.gameOverT1.string = "Game Over", this.gameOverT2.string = "Click to view the score"), this.gameOveEndBool = !0, this.gameOverT1.node.zIndex = 999, this.gameOverT2.node.zIndex = 999, this.gameOverToEnd.zIndex = 999, this.gameOverT1.node.opacity = 0, this.gameOverT1.node.y = 100, this.gameOverToEnd.y = 0, this.gameOverT1.node.runAction(cc.sequence(cc.delayTime(.2), cc.spawn(cc.fadeIn(1), cc.moveBy(1, 0, -50)), cc.delayTime(.3))), this.gameOverToEnd.runAction(cc.sequence(cc.fadeTo(1, 100), cc.callFunc(function() {
+                1 == t ? (this.gameOverT1.string = "\u6e38 \u620f \u7ed3 \u675f", this.gameOverT2.string = "\u70b9 \u51fb \u67e5 \u770b \u5206 \u6570") : 2 == t ? (this.gameOverT1.string = "\u904a \u6232 \u7d50 \u675f", this.gameOverT2.string = "\u9ede \u64ca \u67e5 \u770b \u5206 \u6578") : 4 == t ? (this.gameOverT1.string = "\uac8c\uc784 \uc885\ub8cc", this.gameOverT2.string = "\ud074\ub9ad \ud558\uc5ec \uc810\uc218 \ubcf4\uae30") : (this.gameOverT1.string = "Game Over", this.gameOverT2.string = "Click to view the score"), this.gameOveEndBool = !0, this.gameOverT1.node.zIndex = 999, this.gameOverT2.node.zIndex = 999, this.gameOverToEnd.zIndex = 999, this.gameOverT1.node.opacity = 0, this.gameOverT1.node.y = 100, this.gameOverToEnd.y = 0, this.gameOverT1.node.runAction(cc.sequence(cc.delayTime(.2), cc.spawn(cc.fadeIn(1), cc.moveBy(1, 0, -50)), cc.delayTime(.3))), this.gameOverToEnd.runAction(cc.sequence(cc.fadeTo(1, 100), cc.callFunc(function() {
                     e.gameOverToEnd.getComponent(cc.Button).enabled = !0
                 }))), this.gameOverT2.node.opacity = 0, this.gameOverT2.node.y = this.gameOverT1.node.y - 100, this.gameOverT2.node.runAction(cc.sequence(cc.delayTime(.2), cc.spawn(cc.fadeIn(1), cc.moveBy(1, 0, -50)), cc.delayTime(.3))), this.gameOverT2.node.runAction(cc.sequence(cc.delayTime(2), cc.scaleTo(.3, 1.2).easing(cc.easeSineInOut()), cc.scaleTo(.3, 1).easing(cc.easeSineInOut()))).repeatForever()
             },
@@ -3414,8 +3410,6 @@ console.log('12')
                         t.position = e.position, t.scale = .3, t.runAction(cc.sequence(cc.scaleTo(.5, 1.1), cc.fadeOut(.5)))
                     }, 300 * n)
                 }, t.prototype.addScoreEffect = function() {
-                    console.log('12')
-
                     var e = r.default.Spawn("add1", cc.find("Canvas/mainGameUi"));
                     e.position = cc.find("Canvas/mainGameUi").children[0].position.add(cc.v2(0, 50)), e.runAction(cc.spawn(cc.moveBy(.5, 0, 50), cc.fadeOut(.5)))
                 }, c([h()], t.prototype, "fruitNumber", void 0), t = c([f], t)
@@ -4000,40 +3994,4 @@ console.log('12')
         "./GameFunction": "GameFunction",
         "./GameManager": "GameManager"
     }]
-}, {}, [ "MainGameJS",
-    "AddScore",
-    "AdjusWithHeight",
-    "AudioManager",
-    "DataManager",
-    "EffectCenter",
-    "FailedUi",
-    "GameFunction",
-    "GameManager",
-    "InputController",
-    "KnifeCollision",
-    "MainGameUi",
-    "NewAttribute",
-    "PhysicsParticle",
-    "Rocker",
-    "RotateAround",
-    "Stack",
-    "WallUpdate",
-    "fruitData",
-    "startPanel",
-    "GameConfig",
-    "GameUiTools",
-    "DynamicLoad",
-    "EffectSprite",
-    "PlayerInfo",
-    "PoolManager",
-    "RandomFly",
-    "RenderWater",
-    "Shake",
-    "SpriteManager",
-    "Toast",
-    "Utils",
-    "bfPrefabJs",
-    "gameOverJs",
-    "linkHttpIconJs",
-    "startGameJs",
-    "use_v2.1.x_cc.Action"]);
+}, {}, ["HttpManagerJs", "LanguageSetJs", "LoadSceneJs", "AniTools", "MainGameJS", "ToolsJs", "resArr", "MainManage", "AddScore", "AdjusWithHeight", "AudioManager", "AutoDestroy", "DataManager", "EffectCenter", "FailedUi", "GameFunction", "GameManager", "InputController", "KnifeCollision", "MainGameUi", "NewAttribute", "PhysicsParticle", "Rocker", "RotateAround", "Stack", "WallUpdate", "fruitData", "peaCannonAI", "startPanel", "wineObsmove", "winerCollision", "GameConfig", "GameUiTools", "DynamicLoad", "EffectSprite", "PlayerInfo", "PoolManager", "RandomFly", "RenderWater", "Shake", "SpriteManager", "Toast", "Utils", "bfPrefabJs", "gameOverJs", "linkHttpIconJs", "startGameJs", "use_v2.1.x_cc.Action"]);
